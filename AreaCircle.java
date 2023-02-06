a154520f0dcb9b5fd6b6330f9f0c7a9c5ef506dc
Suraj V S <111676009+SurajUnny@users.noreply.github.com>
2023-02-06 23:08:55 +0530

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.lang.*;
+class Area
+{
+    public static void cal()
+    {
+         DataInputStream dis=new DataInputStream(System.in);
+         try
+         {
+            System.out.print("Enter the radius of the circle : ");
+            double r=Integer.parseInt(dis.readLine());
+            double Area=3.14*r*r;
+            System.out.println("Area of the circle = "+Area);
+         }
+        catch(IOException e){System.out.println(e);}
+        
+    }
+
+}
+class AreaCircle
+{
+    public static void main(String ars[])
+    {
+        Area a=new Area();
+        a.cal();
+    }
+}

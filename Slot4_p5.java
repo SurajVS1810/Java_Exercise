bd1ce06d8c1b5b2c81749239ccc0f90ae1a66eb6
Suraj V S <111676009+SurajUnny@users.noreply.github.com>
2023-02-24 15:38:53 +0530

    new commit

---
+++ Slot4_p5.java
@@ -0,0 +1,7 @@
+//  Create a class “height” having a foot and inch part. Provide functions for read, display, add and multiplying two heights
+import java.io.*;
+import java.lang.*;
+class Slot4_p5
+{
+    
+}

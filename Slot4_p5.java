0f9575cc2a2a2c12acff80ec7c87ec23f79c8182
Suraj V S <111676009+SurajVS1810@users.noreply.github.com>
2023-02-28 04:19:35 +0530

    Add files via upload

--- Slot4_p5.java
+++ Slot4_p5.java
@@ -1,7 +1,8 @@
-//  Create a class “height” having a foot and inch part. Provide functions for read, display, add and multiplying two heights
+//  Create a class height having a foot and inch part. Provide functions for read, display, add and multiplying two heights
 import java.io.*;
 import java.lang.*;
-class height{
+class height
+{
     int foot;
     int inch;
     public void read()
@@ -10,7 +11,7 @@ class height{
             DataInputStream dis = new DataInputStream(System.in);
             System.out.print("Enter the foot of the height : ");
             foot=Integer.parseInt(dis.readLine());
-            System.out.print("\nEnter the inch of the height : ");
+            System.out.print("Enter the inch of the height : ");
             inch=Integer.parseInt(dis.readLine());
         }catch(IOException e){System.out.println(e);}
     }
@@ -24,7 +25,7 @@ class height{
         h.foot=this.foot+h2.foot;
         if((this.inch+h2.inch)>12){
             rem=(this.inch+h2.inch)/12;
-           h.foot=this.foot+rem; 
+           h.foot=h.foot+rem; 
            h.inch=(this.inch+h2.inch)%12;
         }
         else{
@@ -37,11 +38,12 @@ class Slot4_p5
 {
     public static void main(String args[]){
        height h1= new height();
+       System.out.println();
        System.out.println("Enter height 1 details ");
         h1.read();
         h1.disp();
         height h2= new height();
-        System.out.println("Enter height 2 details ");
+        System.out.println("\nEnter height 2 details ");
         h2.read();
         h2.disp();
         h1.add(h2);

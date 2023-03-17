6fc9a7f522678136994198b18c255842542b6320
Suraj V S <111676009+SurajVS1810@users.noreply.github.com>
2023-03-17 23:53:33 +0530

    Add files via upload

---
+++ Slot20_p3.java
@@ -0,0 +1,46 @@
+//Write a JDBC program to implement Transaction Management in the Department table.
+package JDBC;
+import java.sql.*;
+import java.util.Scanner;
+
+public class Slot20_p3{
+
+	public static void main(String[] args) {
+		try
+		{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","mca","mca");
+		con.setAutoCommit(false);
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter deptid : ");
+		String id=sc.nextLine();
+		System.out.println("Enter deptname : ");
+		String name=sc.nextLine();
+		CallableStatement cst=con.prepareCall("{call dep(?,?)");
+		cst.setString(1,id);
+		cst.setString(2,name);
+		cst.executeUpdate();
+		System.out.println("Enter commit to commit the insertion and rollback to rollback : ");
+		String s=sc.nextLine();
+		if(s.equals("commit")) {
+			con.commit();
+			System.out.println("rows inserted");}
+		else {
+			con.rollback();
+			System.out.println("no rows inserted");
+		}	
+		con.close();
+		}catch(Exception e) {System.out.println(e);}
+
+	}
+}
+
+/*Procedure
+ 
+create or replace procedure dep(deptid in varchar2,deptname in varchar2)
+is 
+begin
+insert into dept1 values(deptid,deptname);
+end;
+/
+*/
